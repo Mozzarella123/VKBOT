b08d673bcd3a5dbfbf9b94d910c91fdbb0e41b27
Nikita <Nikita1998_2010@mail.ru>
2017-03-21 00:38:41 +0300

    Подключил новую библиотеку для парсинга. Настроил парсинг основных справочных статей с wikireality.

--- php/libraries/parsing.php
+++ php/libraries/parsing.php
@@ -1,6 +1,7 @@
 <?php
 //Привет
 include 'simple_html_dom.php';
+include 'phpQuery-onefile.php';
 class Parsing
 {
 	private $site_url;
@@ -10,13 +11,34 @@ class Parsing
 		$this->site_url = $site_url;
 		$this->html = file_get_html($site_url);
 	}
+	public function CurlQuery($site_url)
+	{
+		$curl = curl_init($site_url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		$request = curl_exec($curl);
+		curl_close( $curl );
+		return $request;
+	}
 	public function ParseFirstTag($tagname)
 	{
-		$single = $this->html->find($tagname, 0); 
+		$single = $this->html->find($tagname, 0);
 		$res = $single->plaintext;
 		$this->html->clear();
-		unset($this->html); 
+		unset($this->html);
 		return $res;
 	}
+	public function ParseFirst()
+	{
+		$document = phpQuery::newDocument($this->CurlQuery($this->site_url));
+		$urls= $document->find("#mw-pages ul li a");
+		foreach ($urls as $href)
+		{
+			$url = pq($href)->attr('href');
+			$single = phpQuery::newDocument($this->CurlQuery('http://wikireality.ru'.$url));
+			echo $single->find('#firstHeading')->html();
+			echo "<br>";
+			echo $single->find('#mw-content-text > p')->eq(0)->text();
+			echo "<br>";
+		}
+	}
 }
-?>
--- php/test.php
+++ php/test.php
@@ -1,25 +1,22 @@
-<?php 
+<?php
 //Подключаем библиотеки
 include 'libraries/parsing.php';
 include 'libraries/vkapi.php';
 /* $mysqli = new mysqli('localhost', 'cw44342_botvk', '12345qwe','cw44342_botvk');
 $res = $mysqli->query("SELECT MAX(id) FROM vocabulary");
 $row = $res->fetch_assoc(); */
-//���� ������� ���������� 
-
+//Да 
 $client_id = '5930931';
 $scope = 'offline,message,groups,photos,wall';
 $token = '885ccfaf7b19360f038350e419a0e488d208a45ad07be217e99b56b5ce69587b91d2791c86dcb2b8434a1';
-
 /* $token = 'b12ead4cb12ead4cb12cf0ccc1b174d2ffbb12eb12ead4ce9e2be7bba61211c34fd9993'; 
  */
-
 $api = new UserMethods($token);
-        $request = json_decode($api->APIMethod('photos.getUploadServer','album_id=243751267&group_id=142367703&v=5.62'))->response->upload_url;
- 		$img_src = "@".dirname(__FILE__)."/s.jpg";
-		$post_params = array(
-		"file1" => $img_src,
-		);
-		$api->UploadImageToAlbum($post_params,243751267,142367703);
-  echo $request;
-  echo $img_src;
+$request = json_decode($api->APIMethod('photos.getUploadServer','album_id=243751267&group_id=142367703&v=5.62'))->response->upload_url;
+$img_src = "@".dirname(__FILE__)."/s.jpg";
+$post_params = array(
+	"file1" => $img_src,
+);
+/*$api->UploadImageToAlbum($post_params,243751267,142367703);*/
+$parsing =new Parsing("http://wikireality.ru/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9C%D0%B5%D0%BC%D1%8B&pagefrom=%D0%A3%D0%BF%D0%BE%D1%80%D0%BE%D1%82%D1%8B%D0%B9#mw-pages");
+$parsing->ParseFirst();
